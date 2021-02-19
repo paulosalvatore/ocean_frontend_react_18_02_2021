d9293a01c42efdae1bd6fd6078091148da978733
Paulo Salvatore <salvatoregames@gmail.com>
2021-02-18 21:40:26 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Board() {
+    return <div>Board</div>
+}
+
 // Criação do componente Game
 function Game() {
     return (
